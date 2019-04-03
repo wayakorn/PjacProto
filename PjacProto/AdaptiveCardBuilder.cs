1a3b741a21f401479c75aad98c4260c159c95307
Way Vadhanasin <wayvad@microsoft.com>
2019-04-03 15:28:33 -0700

    More update

--- PjacProto/AdaptiveCardBuilder.cs
+++ PjacProto/AdaptiveCardBuilder.cs
@@ -7,31 +7,49 @@ namespace PjacProto
     {
         public class PrintJob
         {
+            public enum JobStatus
+            {
+                Printing,
+                Printed
+            }
+
             public string Name { get; }
             public DateTime Submitted { get; }
-            public string Status { get; }
+            public JobStatus Status { get; }
+            public string IconUrl { get; }
             public string Owner { get; }
             public int Pages { get; }
             public DateTime Completed { get; }
-            public PrintJob(string name, DateTime submitted, string status, string owner, int pages, DateTime completed)
+            public PrintJob(string name, DateTime submitted, JobStatus status, string iconUrl, string owner, int pages, DateTime completed)
             {
                 Name = name;
                 Submitted = submitted;
                 Status = status;
+                IconUrl = iconUrl;
                 Owner = owner;
                 Pages = pages;
                 Completed = completed;
             }
             public override string ToString()
             {
+                string statusText = (Status == JobStatus.Printed) ? "Printed" : "Printing";
+
                 string card = Util.ReadTextAsset("pjac_adaptivecards_template_job.json");
                 card = card.Replace("%%NAME%%", Name);
-                card = card.Replace("%%STATUS%%", Status);
+                card = card.Replace("%%STATUS%%", statusText);
+                card = card.Replace("%%ICON_URL%%", IconUrl);
                 card = card.Replace("%%OWNER%%", Owner);
                 card = card.Replace("%%PAGES%%", Pages.ToString());
-                // Submitted & completed are in this format: 2019-03-30T00:01:00Z
                 card = card.Replace("%%DATE_SUBMITTED%%", Submitted.ToString("s"));
-                card = card.Replace("%%DATE_COMPLETED%%", Completed.ToString("s"));
+
+                string completedJson = "";
+                if (Status == JobStatus.Printed)
+                {
+                    completedJson = Util.ReadTextAsset("pjac_adaptivecards_template_job_completed.json");
+                    completedJson = completedJson.Replace("%%DATE_COMPLETED%%", Completed.ToString("s"));
+                }
+                card = card.Replace("%%COMPLETED_JSON%%", completedJson);
+
                 return card;
             }
         }
@@ -40,14 +58,24 @@ namespace PjacProto
 
         public void AddJob(PrintJob job)
         {
-            m_printJobs.Add(job);
+            m_printJobs.Insert(0, job);
         }
 
         public override string ToString()
         {
             string jobs = "";
+            bool needCommaSeparator = false;
             foreach (var job in m_printJobs)
             {
+                if (needCommaSeparator)
+                {
+                    jobs += ",";
+                }
+                else
+                {
+                    needCommaSeparator = true;
+                }
+
                 jobs += job.ToString();
             }
             string result = Util.ReadTextAsset("pjac_adaptivecards_template_begin.json") + jobs + Util.ReadTextAsset("pjac_adaptivecards_template_end.json");
--- PjacProto/MainPage.xaml.cs
+++ PjacProto/MainPage.xaml.cs
@@ -65,10 +65,176 @@ namespace PjacProto
             myGrid.Children.Clear();
         }
 
+        readonly string[] DocumentNames = new string[]
+        {
+            "Back to the Future",
+            "Desperado",
+            "Night at the Museum",
+            "Robocop",
+            "Ghostbusters",
+            "Cool World",
+            "Donnie Darko",
+            "Double Indemnity",
+            "The Spanish Prisoner",
+            "The Smurfs",
+            "Dead Alive",
+            "Army of Darkness",
+            "Peter Pan",
+            "The Jungle Story",
+            "Red Planet",
+            "Deep Impact",
+            "The Long Kiss Goodnight",
+            "Juno",
+            "(500) Days of Summer",
+            "The Dark Knight",
+            "Bringing Down the House",
+            "Se7en",
+            "Chocolat",
+            "The American",
+            "The American President",
+            "Hudsucker Proxy",
+            "Conan the Barbarian",
+            "Shrek",
+            "The Fox and the Hound",
+            "Lock, Stock, and Two Barrels",
+            "Date Night",
+            "200 Cigarettes",
+            "9 1/2 Weeks",
+            "Iron Man 2",
+            "Tombstone",
+            "Young Guns",
+            "Fight Club",
+            "The Cell",
+            "The Unborn",
+            "Black Christmas",
+            "The Change-Up",
+            "The Last of the Mohicans",
+            "Shutter Island",
+            "Ronin",
+            "Ocean’s 11",
+            "Philadelphia",
+            "Chariots of Fire",
+            "M*A*S*H",
+            "Walking and Talking",
+            "Walking Tall",
+            "The 40 Year Old Virgin",
+            "Superman III",
+            "The Hour",
+            "The Slums of Beverly Hills",
+            "Secretary",
+            "Secretariat",
+            "Pretty Woman",
+            "Sleepless in Seattle",
+            "The Iron Mask",
+            "Smoke",
+            "Schindler’s List",
+            "The Beverly Hillbillies",
+            "The Ugly Truth",
+            "Bounty Hunter",
+            "Say Anything",
+            "8 Seconds",
+            "Metropolis",
+            "Indiana Jones and the Temple of Doom",
+            "Kramer vs. Kramer",
+            "The Manchurian Candidate",
+            "aging Bull",
+            "Heat",
+            "About Schmidt",
+            "Re-Animator",
+            "Evolution",
+            "Gone in 60 Seconds",
+            "Wanted",
+            "The Man with One Red Shoe",
+            "The Jerk",
+            "Whip It",
+            "Spanking the Monkey",
+            "Steel Magnolias",
+            "Horton Hears a Who",
+            "Honey",
+            "Brazil",
+            "Gorillas in the Mist",
+            "Before Sunset",
+            "After Dark",
+            "From Dusk til Dawn",
+            "Cloudy with a Chance of Meatballs",
+            "Harvey",
+            "Mr. Smith Goes to Washington",
+            "L.A. Confidential",
+            "Little Miss Sunshine",
+            "The Future",
+            "Howard the Duck",
+            "Howard’s End",
+            "The Innkeeper",
+            "Revolutionary Road"
+        };
+
+        readonly string[] IconUrls = new string[]
+        {
+            "http://icons.iconarchive.com/icons/dtafalonso/modern-xp/512/ModernXP-31-Filetype-Word-icon.png",
+            "https://cdn.windowsfileviewer.com/images/types/xlsx.png",
+            "https://www.plmworld.org/media/hmvmbwgr.png",
+            "https://cdn2.iconfinder.com/data/icons/picons-basic-1/57/basic1-029_url_web_address-512.png"
+        };
+
+        readonly string[] Owners = new string[]
+        {
+            "Dan Altavilla",
+            "Shawn Armstrong",
+            "Gerson Bautista",
+            "Tim Beckham",
+            "Braden Bishop",
+            "Chasen Bradford",
+            "Jay Bruce",
+            "Roenis Elías",
+            "Edwin Encarnación",
+            "Matt Festa",
+            "David Freitas",
+            "Cory Gearrin",
+            "Marco Gonzales",
+            "Dee Gordon",
+            "Mitch Haniger",
+            "Ryon Healy",
+            "Félix Hernández",
+            "Mike Leake",
+            "Wade LeBlanc",
+            "Tom Murphy",
+            "Omar Narváez",
+            "Zac Rosscup",
+            "Nick Rumbelow",
+            "Domingo Santana",
+            "Kyle Seager",
+            "Mallex Smith",
+            "Hunter Strickland",
+            "Ichiro Suzuki",
+            "Anthony Swarzak",
+            "Sam Tuivailala",
+            "Daniel Vogelbach"
+        };
+
+        private DateTime AddRandomPrintJob(AdaptiveCardBuilder builder, DateTime current, AdaptiveCardBuilder.PrintJob.JobStatus status)
+        {
+            var rnd = new Random((int)(current.Ticks + DateTime.Now.Ticks));
+            int dateTimeDelta = 1 + (rnd.Next() % 10);
+            DateTime newDateTime = current.AddMinutes(dateTimeDelta);
+
+            string iconUrl = IconUrls[(rnd.Next() % IconUrls.Length)];
+            string docName = DocumentNames[(rnd.Next() % DocumentNames.Length)];
+            string owner = Owners[(rnd.Next() % Owners.Length)];
+            int pages = dateTimeDelta;
+
+            builder.AddJob(new AdaptiveCardBuilder.PrintJob(docName, newDateTime, status, iconUrl, owner, pages, newDateTime));
+            return newDateTime;
+        }
+
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
+            DateTime current = DateTime.Parse("2019-04-01T11:11:11Z");
             var builder = new AdaptiveCardBuilder();
-            builder.AddJob(new AdaptiveCardBuilder.PrintJob("Document 1", DateTime.Now, "Printing", "Joe Smith", 10, DateTime.Now));
+            for (int i = 0; i < 10; ++i)
+            {
+                current = AddRandomPrintJob(builder, current, AdaptiveCardBuilder.PrintJob.JobStatus.Printed);
+            }
+            AddRandomPrintJob(builder, current, AdaptiveCardBuilder.PrintJob.JobStatus.Printing);
             string card = builder.ToString();
             _ShowAdaptiveCard(card);
         }
