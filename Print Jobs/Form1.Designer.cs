a968d464b124a01bc6d75f9224786b112a7cb410
Way Vadhanasin <wayvad@microsoft.com>
2019-04-20 16:52:34 -0700

    Add mroe prototypes.

---
+++ Print Jobs/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Print_Jobs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate Random Print Job";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 499);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Print Job Viewer";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Print Jobs/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+using Print_Jobs.ShellHelpers;
+using Microsoft.Win32;
+
+namespace Print_Jobs
+{
+    public partial class Form1 : Form
+    {
+        readonly string AppName = Assembly.GetExecutingAssembly().GetName().Name;
+        readonly string AppPath = Assembly.GetExecutingAssembly().Location;
+        readonly string UrlProtocol;
+
+        public Form1()
+        {
+            UrlProtocol = AppName.Replace(' ', '-');
+
+            InitializeComponent();
+
+///            RegisterProtocol();
+            RegisterShortcut();
+        }
+
+        private void RegisterShortcut()
+        {
+            string shortcutPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + $"\\Microsoft\\Windows\\Start Menu\\Programs\\{AppName}.lnk";
+            if (!File.Exists(shortcutPath))
+            {
+                IShellLinkW newShortcut = (IShellLinkW)new CShellLink();
+                // Create a shortcut to the exe
+                newShortcut.SetPath(AppPath);
+
+                // Open the shortcut property store, set the AppUserModelId property
+                IPropertyStore newShortcutProperties = (IPropertyStore)newShortcut;
+
+                PropVariantHelper varAppId = new PropVariantHelper();
+
+                varAppId.SetValue(AppName);
+                newShortcutProperties.SetValue(PROPERTYKEY.AppUserModel_ID, varAppId.Propvariant);
+
+                // Commit the shortcut to disk
+                IPersistFile newShortcutSave = (IPersistFile)newShortcut;
+                newShortcutSave.Save(shortcutPath, true);
+            }
+        }
+
+        private void UnregisterShortcut()
+        {
+            string shortcutPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + $"\\Microsoft\\Windows\\Start Menu\\Programs\\{AppName}.lnk";
+            if (File.Exists(shortcutPath))
+            {
+                File.Delete(shortcutPath);
+            }
+        }
+
+/*
+        private void RegisterProtocol()
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\Classes\\" + UrlProtocol);
+            if (key == null)
+            {
+                key = Registry.CurrentUser.CreateSubKey("Software\\Classes\\" + UrlProtocol);
+                key.SetValue(string.Empty, "URL: " + UrlProtocol);
+                key.SetValue("URL Protocol", string.Empty);
+
+                key = key.CreateSubKey(@"shell\open\command");
+                key.SetValue(string.Empty, $@"{AppPath} ""%1"""); // %1 represents the argument - this tells windows to open this program with an argument / parameter
+            }
+
+            key.Close();
+        }
+
+        private void UnregisterProtocol()
+        {
+            Registry.CurrentUser.DeleteSubKeyTree("Software\\Classes\\" + UrlProtocol, false);
+        }
+*/
+
+        private void ShowPrintJob(string name, string details, string filename)
+        {
+            string xml =
+                $@"<toast activationType='protocol' launch='file:///{filename}'>
+                    <visual>
+                        <binding template='ToastGeneric'>
+                            <text>""{name}"" sent to printer</text>
+                            <text>{details}</text>
+                        </binding>
+                    </visual>
+                </toast>";
+
+            var toastXml = new XmlDocument();
+            toastXml.LoadXml(xml);
+
+            var toast = new ToastNotification(toastXml);
+            ToastNotificationManager.CreateToastNotifier(AppName).Show(toast);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+///            UnregisterProtocol();
+            UnregisterShortcut();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ShowPrintJob("My Document", string.Format("Submitted at {0}", DateTime.Now), @"c:/temp/test.pdf");
+        }
+    }
+}
