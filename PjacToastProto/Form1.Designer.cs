799306ad132a11bc009902678a7b5fa6b1390880
Way Vadhanasin <wayvad@microsoft.com>
2019-04-21 22:24:19 -0700

    Forked PjacToastProto.

---
+++ PjacToastProto/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Print_Jobs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(416, 88);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate a random print job";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button2.Location = new System.Drawing.Point(0, 698);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(830, 48);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Clear Status";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(830, 698);
+            this.textBox1.TabIndex = 1;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button3);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Size = new System.Drawing.Size(1250, 746);
+            this.splitContainer1.SplitterDistance = 416;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.Location = new System.Drawing.Point(0, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(416, 100);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Complete all print jobs";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1250, 746);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "PJAC Toast Prototype";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ PjacToastProto/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+using Print_Jobs.ShellHelpers;
+using Microsoft.Win32;
+
+namespace Print_Jobs
+{
+    public partial class Form1 : Form
+    {
+        readonly string AppName = Assembly.GetExecutingAssembly().GetName().Name;
+        readonly string AppPath = Assembly.GetExecutingAssembly().Location;
+        readonly string UrlProtocol;
+
+        public Form1()
+        {
+            UrlProtocol = AppName.Replace(' ', '-');
+
+            InitializeComponent();
+
+            RegisterProtocol();
+            RegisterShortcut();
+        }
+
+        private void RegisterShortcut()
+        {
+            string shortcutPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + $"\\Microsoft\\Windows\\Start Menu\\Programs\\{AppName}.lnk";
+            if (!File.Exists(shortcutPath))
+            {
+                IShellLinkW newShortcut = (IShellLinkW)new CShellLink();
+                // Create a shortcut to the exe
+                newShortcut.SetPath(AppPath);
+
+                // Open the shortcut property store, set the AppUserModelId property
+                IPropertyStore newShortcutProperties = (IPropertyStore)newShortcut;
+
+                PropVariantHelper varAppId = new PropVariantHelper();
+
+                varAppId.SetValue(AppName);
+                newShortcutProperties.SetValue(PROPERTYKEY.AppUserModel_ID, varAppId.Propvariant);
+
+                // Commit the shortcut to disk
+                IPersistFile newShortcutSave = (IPersistFile)newShortcut;
+                newShortcutSave.Save(shortcutPath, true);
+            }
+        }
+
+        private void UnregisterShortcut()
+        {
+            string shortcutPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + $"\\Microsoft\\Windows\\Start Menu\\Programs\\{AppName}.lnk";
+            if (File.Exists(shortcutPath))
+            {
+                File.Delete(shortcutPath);
+            }
+        }
+
+        private void RegisterProtocol()
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\Classes\\" + UrlProtocol);
+            if (key == null)
+            {
+                key = Registry.CurrentUser.CreateSubKey("Software\\Classes\\" + UrlProtocol);
+                key.SetValue(string.Empty, "URL: " + UrlProtocol);
+                key.SetValue("URL Protocol", string.Empty);
+
+                key = key.CreateSubKey(@"shell\open\command");
+                key.SetValue(string.Empty, $@"{AppPath} ""%1""");
+            }
+
+            key.Close();
+        }
+
+        private void UnregisterProtocol()
+        {
+            Registry.CurrentUser.DeleteSubKeyTree("Software\\Classes\\" + UrlProtocol, false);
+        }
+
+        int m_jobId = 0;
+        readonly List<int> m_activeJobs = new List<int>();
+
+        private void CreatePrintJob(string name)
+        {
+            m_jobId++;
+            m_activeJobs.Add(m_jobId);
+
+            string submitted = DateTime.Now.ToString();
+            string xml =
+                $@"<toast scenario='reminder' activationType='protocol' launch='{UrlProtocol}'>
+                    <visual>
+                        <binding template='ToastGeneric'>
+                            <text>Printing ""{name}""...</text>
+                            <text>Job {m_jobId}, submitted {submitted}</text>
+                        </binding>
+                    </visual>
+                    <actions>
+                        <action
+                            content='Cancel job {m_jobId}'
+                            activationType='foreground'
+                            arguments='check'/>
+                    </actions>
+                </toast>";
+
+            var toastXml = new XmlDocument();
+            toastXml.LoadXml(xml);
+
+            DateTimeOffset expirationTime = DateTime.Now.AddMinutes(1);
+
+            var toast = new ToastNotification(toastXml)
+            {
+                ExpirationTime = expirationTime,
+                Group = AppName,
+                Tag = m_jobId.ToString()
+            };
+
+            toast.Activated += Toast_Activated;
+
+            ToastNotificationManager.CreateToastNotifier(AppName).Show(toast);
+            AddStatus($@"Printing job {m_jobId} ""{name}""...");
+        }
+
+        private void AddStatus(string line)
+        {
+            List<string> lines = new List<string>(textBox1.Lines);
+            lines.Add(line);
+            textBox1.Lines = lines.ToArray();
+        }
+
+        private void Toast_Activated(ToastNotification sender, object args)
+        {
+            Invoke(new Action(() =>
+            {
+                AddStatus(string.Format("Job {0} cancelled.", sender.Tag));
+            }));
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            UnregisterProtocol();
+            UnregisterShortcut();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CreatePrintJob(GetRandomName());
+            timer1.Enabled = true;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (var notif in ToastNotificationManager.History.GetHistory(AppName))
+            {
+                AddStatus($"Job {notif.Tag} completed (removed from Action Center).");
+                ToastNotificationManager.History.Remove(notif.Tag, AppName, AppName);
+            }
+        }
+
+        private string GetRandomName()
+        {
+            string[] DocumentNames = new string[]
+            {
+                "Back to the Future",
+                "Desperado",
+                "Night at the Museum",
+                "Robocop",
+                "Ghostbusters",
+                "Cool World",
+                "Donnie Darko",
+                "Double Indemnity",
+                "The Spanish Prisoner",
+                "The Smurfs",
+                "Dead Alive",
+                "Army of Darkness",
+                "Peter Pan",
+                "The Jungle Story",
+                "Red Planet",
+                "Deep Impact",
+                "The Long Kiss Goodnight",
+                "Juno",
+                "(500) Days of Summer",
+                "The Dark Knight",
+                "Bringing Down the House",
+                "Se7en",
+                "Chocolat",
+                "The American",
+                "The American President",
+                "Hudsucker Proxy",
+                "Conan the Barbarian",
+                "Shrek",
+                "The Fox and the Hound",
+                "Lock, Stock, and Two Barrels",
+                "Date Night",
+                "200 Cigarettes",
+                "9 1/2 Weeks",
+                "Iron Man 2",
+                "Tombstone",
+                "Young Guns",
+                "Fight Club",
+                "The Cell",
+                "The Unborn",
+                "Black Christmas",
+                "The Change-Up",
+                "The Last of the Mohicans",
+                "Shutter Island",
+                "Ronin",
+                "Ocean’s 11",
+                "Philadelphia",
+                "Chariots of Fire",
+                "M*A*S*H",
+                "Walking and Talking",
+                "Walking Tall",
+                "The 40 Year Old Virgin",
+                "Superman III",
+                "The Hour",
+                "The Slums of Beverly Hills",
+                "Secretary",
+                "Secretariat",
+                "Pretty Woman",
+                "Sleepless in Seattle",
+                "The Iron Mask",
+                "Smoke",
+                "Schindler’s List",
+                "The Beverly Hillbillies",
+                "The Ugly Truth",
+                "Bounty Hunter",
+                "Say Anything",
+                "8 Seconds",
+                "Metropolis",
+                "Indiana Jones and the Temple of Doom",
+                "Kramer vs. Kramer",
+                "The Manchurian Candidate",
+                "aging Bull",
+                "Heat",
+                "About Schmidt",
+                "Re-Animator",
+                "Evolution",
+                "Gone in 60 Seconds",
+                "Wanted",
+                "The Man with One Red Shoe",
+                "The Jerk",
+                "Whip It",
+                "Spanking the Monkey",
+                "Steel Magnolias",
+                "Horton Hears a Who",
+                "Honey",
+                "Brazil",
+                "Gorillas in the Mist",
+                "Before Sunset",
+                "After Dark",
+                "From Dusk til Dawn",
+                "Cloudy with a Chance of Meatballs",
+                "Harvey",
+                "Mr. Smith Goes to Washington",
+                "L.A. Confidential",
+                "Little Miss Sunshine",
+                "The Future",
+                "Howard the Duck",
+                "Howard’s End",
+                "The Innkeeper",
+                "Revolutionary Road"
+            };
+
+            var rnd = new Random((int)(DateTime.Now.Ticks % int.MaxValue));
+            string name = DocumentNames[(rnd.Next() % DocumentNames.Length)];
+            return name;
+        }
+    }
+}
