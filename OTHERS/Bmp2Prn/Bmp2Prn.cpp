60ccc5fb70c7bfc9d00c8db39b36f5aed3e74ff6
Way Vadhanasin <wayvad@microsoft.com>
2019-04-30 10:04:43 -0700

    Now builds, but runtime failing on AddPage!

--- OTHERS/Bmp2Prn/Bmp2Prn.cpp
+++ OTHERS/Bmp2Prn/Bmp2Prn.cpp
@@ -15,10 +15,232 @@ const float DEFAULT_DPI = 96.0f;
 #include <Windows.h>
 #include <WinUser.h>
 
+#include <atlcomcli.h>
+
 #include <iostream>
 
 #define CHECK(cond) \
-  { if (!cond) { DebugBreak(); } abort; }
+  { if (!cond) { std::cout << GetLastError() << std::endl; DebugBreak(); } abort; }
+
+
+SIZE MyGetPageSize(const wchar_t* printerName)
+{
+	SIZE result;
+
+	HANDLE hPrinter;
+	CHECK(OpenPrinter((LPWSTR)printerName, &hPrinter, nullptr));
+
+	DWORD printerInfoBufferBytes;
+	CHECK(!GetPrinter(hPrinter, 8, nullptr, 0, &printerInfoBufferBytes) && GetLastError() == ERROR_INSUFFICIENT_BUFFER);
+
+	BYTE * printerInfoBuffer = new BYTE[printerInfoBufferBytes];
+	CHECK(GetPrinter(hPrinter, 8, printerInfoBuffer, printerInfoBufferBytes, &printerInfoBufferBytes));
+	CHECK((size_t)printerInfoBufferBytes >= sizeof(PRINTER_INFO_8));
+
+	PRINTER_INFO_8 * printerInfo = (PRINTER_INFO_8*)printerInfoBuffer;
+	result.cx = printerInfo->pDevMode->dmPaperWidth;
+	result.cy = printerInfo->pDevMode->dmPaperLength;
+
+	std::wcout << L"Printer info: name=\"" << printerName << L"\"" <<
+		L", width=" << result.cx <<
+		L", length=" << result.cy << L"." << std::endl;
+
+	delete[] printerInfoBuffer;
+	ClosePrinter(hPrinter);
+
+	return result;
+}
+
+void PrintBitmap(const wchar_t* printerName, const wchar_t* bmpFilePath)
+{
+	HRESULT hr;
+
+	SIZE paperSize = MyGetPageSize(PRINTERNAME);
+
+	SIZE bitmapSize = { 0 };
+	{
+		HBITMAP hBitmap = (HBITMAP)LoadImage(NULL, bmpFilePath, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+		CHECK(hBitmap);
+
+		BITMAP bm;
+		int objectBytes = GetObjectW(hBitmap, sizeof(BITMAP), (LPVOID)&bm);
+		CHECK((size_t)objectBytes == sizeof(BITMAP));
+
+		bitmapSize.cx = bm.bmWidth;
+		bitmapSize.cy = bm.bmHeight;
+		DeleteObject(hBitmap);
+	}
+
+	// Create WIC factory
+	CComPtr<IWICImagingFactory> wicFactory;
+	hr = CoCreateInstance(
+		CLSID_WICImagingFactory,
+		nullptr,
+		CLSCTX_INPROC_SERVER,
+		IID_PPV_ARGS(&wicFactory)
+	);
+	CHECK(SUCCEEDED(hr));
+	CHECK(wicFactory);
+
+	CComPtr<IWICBitmapDecoder> decoder;
+	hr = wicFactory->CreateDecoderFromFilename(
+		bmpFilePath,                     // Image to be decoded
+		nullptr,                         // Do not prefer a particular vendor
+		GENERIC_READ,                    // Desired read access to the file
+		WICDecodeMetadataCacheOnDemand,  // Cache metadata when needed
+		&decoder                         // Pointer to the decoder
+	);
+	CHECK(SUCCEEDED(hr));
+	CHECK(decoder);
+
+	CComPtr<IWICBitmapFrameDecode> frame;
+	hr = decoder->GetFrame(0, &frame);
+	CHECK(SUCCEEDED(hr));
+	CHECK(frame);
+
+	CComPtr<IWICFormatConverter> convertedSourceBitmap;
+	hr = wicFactory->CreateFormatConverter(&convertedSourceBitmap);
+	CHECK(SUCCEEDED(hr));
+	CHECK(convertedSourceBitmap);
+
+	hr = convertedSourceBitmap->Initialize(
+		frame,                           // Input bitmap to convert
+		GUID_WICPixelFormat32bppPBGRA,   // Destination pixel format
+		WICBitmapDitherTypeNone,         // Specified dither pattern
+		nullptr,                         // Specify a particular palette 
+		0.f,                             // Alpha threshold
+		WICBitmapPaletteTypeCustom       // Palette translation type
+	);
+	CHECK(SUCCEEDED(hr));
+
+	CComPtr<ID2D1Device> d2dDevice;
+	{
+		D2D1_FACTORY_OPTIONS options = {};
+#if defined(_DEBUG)
+		options.debugLevel = D2D1_DEBUG_LEVEL_INFORMATION;
+#endif
+		CComPtr<ID2D1Factory1> d2dFactory;
+		hr = D2D1CreateFactory(
+			D2D1_FACTORY_TYPE_SINGLE_THREADED,
+			options,
+			&d2dFactory
+		);
+		CHECK(SUCCEEDED(hr));
+		CHECK(d2dFactory);
+
+		UINT createDeviceFlags = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
+#ifdef _DEBUG
+		createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+		CComPtr <ID3D11Device> d3dDevice;
+		hr = D3D11CreateDevice(
+			nullptr,       // use default adapter
+			D3D_DRIVER_TYPE_HARDWARE,
+			nullptr,       // no external software rasterizer
+			createDeviceFlags,
+			nullptr,       // use default set of feature levels
+			0,
+			D3D11_SDK_VERSION,
+			&d3dDevice,
+			nullptr,       // do not care about what feature level is chosen
+			nullptr        // do not retain D3D device context
+		);
+		CHECK(SUCCEEDED(hr));
+		CHECK(d3dDevice);
+
+		// Get a DXGI device interface from the D3D device
+		CComPtr <IDXGIDevice> dxgiDevice;
+		hr = d3dDevice->QueryInterface(&dxgiDevice);
+		CHECK(SUCCEEDED(hr));
+		CHECK(dxgiDevice);
+
+		// Create a D2D device from the DXGI device
+		hr = d2dFactory->CreateDevice(
+			dxgiDevice,
+			&d2dDevice
+		);
+		CHECK(SUCCEEDED(hr));
+		CHECK(d2dDevice);
+	}
+
+	// Print stuff in here
+	CComPtr<ID2D1PrintControl> printControl;
+	{
+		// Create a factory for document print job
+		CComPtr<IPrintDocumentPackageTargetFactory> documentTargetFactory;
+		hr = ::CoCreateInstance(
+			__uuidof(PrintDocumentPackageTargetFactory),
+			nullptr,
+			CLSCTX_INPROC_SERVER,
+			IID_PPV_ARGS(&documentTargetFactory)
+		);
+		CHECK(SUCCEEDED(hr));
+		CHECK(documentTargetFactory);
+
+		// Create the XPS document target
+		CComPtr <IPrintDocumentPackageTarget> documentTarget;
+		hr = documentTargetFactory->CreateDocumentPackageTargetForPrintJob(
+			printerName,                                // printer name
+			L"Direct2D desktop app printing sample",    // job name
+			nullptr,                                    // job output stream; when nullptr, send to printer
+			nullptr,                                    // job print ticket
+			&documentTarget                             // result IPrintDocumentPackageTarget object
+		);
+		CHECK(SUCCEEDED(hr));
+		CHECK(documentTarget);
+
+		// Create the D2D print control
+		hr = d2dDevice->CreatePrintControl(
+			wicFactory,
+			documentTarget,
+			nullptr,
+			&printControl
+		);
+		CHECK(SUCCEEDED(hr));
+		CHECK(printControl);
+	}
+
+	// Create a D2D Device Context dedicated for the print job
+	CComPtr<ID2D1DeviceContext> d2dContextForPrint;
+	hr = d2dDevice->CreateDeviceContext(
+		D2D1_DEVICE_CONTEXT_OPTIONS_NONE,
+		&d2dContextForPrint
+	);
+	CHECK(SUCCEEDED(hr));
+	CHECK(d2dContextForPrint);
+
+	// Create D2D commandList
+	CComPtr <ID2D1CommandList> commandList;
+	hr = d2dContextForPrint->CreateCommandList(&commandList);
+	CHECK(SUCCEEDED(hr));
+	CHECK(commandList);
+
+	// Set the target commandList, which will get played back later
+	d2dContextForPrint->SetTarget(commandList);
+
+	// Create D2D bitmap
+	CComPtr<ID2D1Bitmap> d2dBitmap;
+	hr = d2dContextForPrint->CreateBitmapFromWicBitmap(convertedSourceBitmap, &d2dBitmap);
+	CHECK(SUCCEEDED(hr));
+	CHECK(d2dBitmap);
+
+	// Calculate the margin
+	const float margin = (2 * PAGE_MARGIN_IN_DIPS);
+
+	// Draw the page into the commandList
+	d2dContextForPrint->BeginDraw();
+	d2dContextForPrint->DrawBitmap(d2dBitmap, D2D1::RectF(margin, margin, bitmapSize.cx, bitmapSize.cy));
+	d2dContextForPrint->EndDraw();
+
+	// Playback the commandList onto the page and finalize it
+	hr = printControl->AddPage(commandList, D2D1::SizeF(paperSize.cx, paperSize.cy), nullptr);
+	CHECK(SUCCEEDED(hr));
+	hr = printControl->Close();
+	CHECK(SUCCEEDED(hr));
+}
+
+#if 0
 
 // Note: outBuffer needs to be deleted with the delete[] operator
 HBITMAP MyLoadBitmap(const wchar_t* bmpFilePath, SIZE* outImageSizeInPx, BYTE** outBuffer, int* outBufferBytes)
@@ -40,42 +262,14 @@ HBITMAP MyLoadBitmap(const wchar_t* bmpFilePath, SIZE* outImageSizeInPx, BYTE** 
 	CHECK(*outBuffer);
 	CHECK(::GetBitmapBits(hBitmap, *outBufferBytes, (LPVOID)*outBuffer) == *outBufferBytes);
 
-	std::wcout << L"Bitmap loaded from \"" << bmpFilePath << L"\"" << 
+	std::wcout << L"Bitmap loaded from \"" << bmpFilePath << L"\"" <<
 		L", dimension in pixels: " << outImageSizeInPx->cx << ", " << outImageSizeInPx->cy <<
 		L" (" << *outBufferBytes << L" bytes)." << std::endl;
 
 	return hBitmap;
 }
 
-SIZE MyGetPageSize(const wchar_t* printerName)
-{
-	SIZE result;
-
-	HANDLE hPrinter;
-	CHECK(OpenPrinter((LPWSTR)printerName, &hPrinter, nullptr));
-
-	DWORD printerInfoBufferBytes;
-	CHECK(!GetPrinter(hPrinter, 8, nullptr, 0, &printerInfoBufferBytes) && GetLastError() == ERROR_INSUFFICIENT_BUFFER);
-
-	BYTE * printerInfoBuffer = new BYTE[printerInfoBufferBytes];
-	CHECK(GetPrinter(hPrinter, 8, printerInfoBuffer, printerInfoBufferBytes, &printerInfoBufferBytes));
-	CHECK(printerInfoBufferBytes >= sizeof(PRINTER_INFO_8));
-
-	PRINTER_INFO_8 * printerInfo = (PRINTER_INFO_8*)printerInfoBuffer;
-	result.cx = printerInfo->pDevMode->dmPaperWidth;
-	result.cy = printerInfo->pDevMode->dmPaperLength;
-
-	std::wcout << L"Printer info: name=\"" << printerName << L"\"" <<
-		L", width=" << result.cx <<
-		L", length=" << result.cy << L"." << std::endl;
-
-	delete[] printerInfoBuffer;
-	ClosePrinter(hPrinter);
-
-	return result;
-}
-
-HRESULT MyCreateDeviceContext(const wchar_t* printerName, SIZE paperSize, SIZE bitmapSizeInPx, HBITMAP hBitmap)
+HRESULT MyCreateDeviceContext_YIKES(const wchar_t* printerName, SIZE paperSize, SIZE bitmapSizeInPx, HBITMAP hBitmap)
 {
 	HRESULT hr;
 
@@ -231,6 +425,8 @@ HRESULT MyCreateDeviceContext(const wchar_t* printerName, SIZE paperSize, SIZE b
 	imagableArea.width = paperSize.cx - (2 * PAGE_MARGIN_IN_DIPS);
 	imagableArea.height = paperSize.cy - (2 * PAGE_MARGIN_IN_DIPS);
 
+
+	/*
 	// Create the surface to render to
 	IDXGISurface* surface;
 	DXGI_SURFACE_DESC surfaceDesc = { 0 };
@@ -265,6 +461,7 @@ HRESULT MyCreateDeviceContext(const wchar_t* printerName, SIZE paperSize, SIZE b
 	hr = renderTarget->CreateBitmapFromWicBitmap(wicBitmap, &d2dBitmap);
 	CHECK(SUCCEEDED(hr));
 	CHECK(d2dBitmap);
+	*/
 
 	// Draw the page into the commandList
 	d2dContextForPrint->BeginDraw();
@@ -276,6 +473,7 @@ HRESULT MyCreateDeviceContext(const wchar_t* printerName, SIZE paperSize, SIZE b
 	CHECK(SUCCEEDED(hr));
 	hr = printControl->Close();
 	CHECK(SUCCEEDED(hr));
+
 
 	// Cleanup
 	d2dBitmap->Release();
@@ -318,4 +516,11 @@ int main()
 
 	delete[] bmpBuffer;
 	DeleteObject(hBitmap);
+}
+#endif
+
+int main()
+{
+	CHECK(SUCCEEDED(::CoInitialize(nullptr)));
+	PrintBitmap(PRINTERNAME, FILEPATH);
 }
